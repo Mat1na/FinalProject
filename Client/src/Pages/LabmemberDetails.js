e588ba811bd728cad9c04cc3073159dd6c9bc705
Mat1na <Stamatia.Bampa@Student.intecbrussel.be>
2022-11-15 21:52:23 +0100

    labmember detail page works

--- Client/src/Pages/LabmemberDetails.js
+++ Client/src/Pages/LabmemberDetails.js
@@ -1,15 +1,64 @@
 import React, { useEffect, useState } from 'react'
+import { Container } from 'react-bootstrap';
 import { useParams } from 'react-router-dom'
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
 
 
 function LabmemberDetails() {
   const { lab } = useParams()
+  const [member, setMember] = useState([]);
+  const [interestsList, setInterestList] = useState([]);
+
+  const fetchLabmember = async () => {
+
+    let res = await fetch("http://localhost:3001/labmembers/fetch-labmembers");
+    let data = await res.json();
+    let labmeberParam = lab.split("-").toString()
+    if (res.ok) {
+      var filtereddata = data.find(item => item.membername.toLowerCase().split(" ").toString() === labmeberParam);
+      setMember(filtereddata);
+      setInterestList(filtereddata.interests)
+      console.log(labmeberParam);
+      console.log(filtereddata)
+      console.log(filtereddata.interests)
+        ;
+    }
+
+  };
+  useEffect(() => {
+    fetchLabmember();
+  }, [lab]);
 
   return (
     <>
-      <div className="text-center" >
-      <h1>{lab}</h1>
-      </div>
+      <Container>
+        <div className="member-details pt-5">
+          <img
+            src={`${member.image}`}
+            className="member-details"
+          />
+        </div>
+        <div className="text-center ">
+          <h2 className="pt-3">
+            {member.membername}
+          </h2>
+          <p className="">
+            {member.functionbasic}
+          </p>
+          <p className="">
+            {member.functionextra}
+          </p>
+        </div>
+        <div className="w">
+  {interestsList.map(item => (
+    <li>
+      {item.interest}
+    </li>
+  ))}
+        </div>
+
+      </Container>
 
     </>
 
