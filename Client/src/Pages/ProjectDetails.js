88271690f5af2084e820e24767bfcf90757bfd8a
5tamatia <Stamatia.Bampa@Student.intecbrussel.be>
2022-11-17 13:44:48 +0100

    project pages & alumni

--- Client/src/Pages/ProjectDetails.js
+++ Client/src/Pages/ProjectDetails.js
@@ -1,34 +1,41 @@
-import React, { useEffect } from 'react'
-import { useState } from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { Card, Container } from "react-bootstrap";
+import { useParams } from "react-router-dom";
 
 function ProjectDetails() {
-const {proj}= useParams()
-const [project, setProject]=useState([])
-const [researchersList,setResearchersList]=useState([])
-const fetchProject = async () => {
-  let res = await fetch("http://localhost:3001/projects/fetch-projects");
-  let data = await res.json();
-  let projectParam = proj.split("-").toString();
-  if (res.ok) {
-    var filtereddata = data.find(
-      (item) =>
-console.log(item.title.toLowerCase().split(" ").toString()) 
-    );
-  console.log(projectParam);
-  console.log(filtereddata );
-  console.log(filtereddata .researchers);
-  // setProject(data);
-  // setResearchersList(data.researchers)
+  const { proj } = useParams();
+  const [project, setProject] = useState([]);
+  const [researchersList, setResearchersList] = useState([]);
+  const fetchProject = async () => {
+    let res = await fetch("http://localhost:3001/projects/fetch-projects");
+    let data = await res.json();
+    let projectParam = proj.split("-").toString();
+    if (res.ok) {
+      var filtereddata = data.find((item) =>item.title.toLowerCase().split(/[\s-]+/).toString() === projectParam);
+      console.log("param:", proj);
+      console.log(filtereddata);
+      setProject(filtereddata)
+      // setResearchersList(filtereddata.researchers)
     }
-};
-useEffect(() => {
-  fetchProject();
-}, [proj]);
+  };
+  useEffect(() => {
+    fetchProject();
+  }, [proj]);
 
   return (
-    <div>Projects</div>
-  )
+    <Container>
+      <div className="m-5">
+        <Card className="">
+        <Card.Title><h1 className="text-center m-5">{project.title}</h1></Card.Title>
+          <Card.Img variant="top p-5" src={project.image} />
+          <Card.Body>
+            <Card.Text className="p-5">{project.content}</Card.Text>
+           </Card.Body>
+        </Card>
+      </div>
+    </Container>
+  );
 }
 
-export default ProjectDetails
+export default ProjectDetails;
