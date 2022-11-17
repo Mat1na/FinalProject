151d3b9c1738029134e1bcfb06c10254a41a11ae
WinandVM <winand_vanmeerbeek@hotmail.com>
2022-11-17 13:37:01 +0100

    Publication section layout hover effect and publication link

--- Client/src/HompageComponents/PublicationSection.js
+++ Client/src/HompageComponents/PublicationSection.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 
 function PublicationSection() {
   const [lastPublications, setLastPublications] = useState([]);
+
   const fetchLastPublications = async () => {
     let res = await fetch("http://localhost:3001/publications/fetch-publications");
     let data = await res.json();
@@ -15,15 +16,25 @@ function PublicationSection() {
     fetchLastPublications();
   }, []);
 
+  //for hover effect
+  const [isHovering, setIsHovering] = useState(false);
+  const handleMouseOver = () => {
+    setIsHovering(true);
+  };
+  const handleMouseOut = () => {
+    setIsHovering(false);
+  };
 
   return (
     <Container>
       <Row>
         <Col className="col-lg-4"><h1>Recent Publications</h1></Col>
         <Col className="col-lg-8">
-          <div><Link to={"/publicationlist"} className='allpublications'>Check out all publications</Link></div>
+          <div><Link to={"/publicationlist"} className='allpublications'><svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" className="bi bi-info-circle-fill me-2" viewBox="0 0 16 16">
+  <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
+</svg>Check out all publications</Link></div>
           {lastPublications.length > 0 && lastPublications.map((publication, index) => {
-            return <div key={publication.order}><Link to={`/publication/${publication.journal.replace(/[^a-zA-Z0-9]/g, '-').toLowerCase()}-${publication.order}`} className="stretched-link journallink">
+            return <div key={publication.order} className="publicationhover mb-4"><Link to={`/publication/${publication.journal.replace(/[^a-zA-Z0-9]/g, '-').toLowerCase()}-${publication.order}`} className="stretched-link journallink">
               <div className="journalcontainer">
 
                 <div>
