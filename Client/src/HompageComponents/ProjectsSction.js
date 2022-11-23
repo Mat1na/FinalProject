8e9c400a398df5aba546723b79a95f266a79fb23
Mat1na <Stamatia.Bampa@Student.intecbrussel.be>
2022-11-23 13:17:07 +0100

    PROJECT OVERLAY

--- Client/src/HompageComponents/ProjectsSction.js
+++ Client/src/HompageComponents/ProjectsSction.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Button, Card, Container } from "react-bootstrap";
+import { Button, Card, Col, Container, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 function ProjectsSction() {
@@ -16,26 +16,46 @@ function ProjectsSction() {
 
   return (
     <>
-     <Container>
-     <h1 className="text-center p-5">Research projects</h1>
-      <div className="d-flex flex-wrap justify-content-center">
-        {projects.map((project) => (
-          <div className="m-3">
-            <Card  className="project-card">
+      <Container>
+        <h1 className="text-center p-5 lab-title">Research projects</h1>
+        <div className="d-flex flex-wrap justify-content-center">
+          <Row>
+          {projects.map((project) => (
+            <>
+             {/* <div className="m-3">
+             <Card  className="project-card">
               <Link to={`/project/${project.title.replace(/\s/g, '-').toLowerCase()}`}>
               <Card.Img variant="top" className="" src={project.image} />
-              </Link>
+             </Link>
               <Card.Body>
-                <Card.Title>{project.title}</Card.Title>
+             <Card.Title>{project.title}</Card.Title>
                 <Card.Text>{project.summary}</Card.Text>
                 <Card.Link href={`/project/${project.title.replace(/\s/g, '-').toLowerCase()}`} >Read more</Card.Link>
               </Card.Body>
-           
-            </Card>
-          </div>
-        ))}
-      </div>
-     </Container>
+            </Card> 
+            </div> */}
+            <Col md={4}>
+   
+              <div className=" projects d-flex justify-content-center align-items-center">
+                <img
+                  src={project.image}
+                  className="project-photo" alt={project.title}
+                />
+                <div className="project-photo-overlay">
+          
+                  <p className="roboto">
+                  {project.title}
+                  </p>
+                </div>
+              </div>
+      
+            </Col>
+            </>
+          ))}
+          </Row>
+        </div>
+        
+      </Container>
     </>
   );
 }
