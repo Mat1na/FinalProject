8f6712f7a91cfdb7d94e478d5564a3815d470ae4
Mat1na <Stamatia.Bampa@Student.intecbrussel.be>
2022-11-23 19:28:52 +0100

    fucking project section hover

--- Client/src/HompageComponents/LabmembersSection.js
+++ Client/src/HompageComponents/LabmembersSection.js
@@ -26,7 +26,7 @@ function LabmembersSection() {
       <Container fluid>
         <h1 className="pb-5 montserrat lab-title">Lab members</h1>
         <Row className="p-3 d-flex  text-center justify-content-center align-items-center">
-{/* Principal Investigator Section*/}
+          {/* Principal Investigator Section*/}
           <h2 className="p-3 montserrat">Principal Investigator</h2>
           {labmemberList.map((member, index) => {
             return (
@@ -35,19 +35,20 @@ function LabmembersSection() {
                   // Grid system with shifting number of elements
                   <>
                     <Col
-                      md={12}
+                      md={5}
                       className="d-flex justify-content-center align-items-center"
                     >
                       <Link to={`/labmember/${member.membername.replace(/\s/g, '-').toLowerCase()}`} className="photo-link">
-
                         <div className=" members d-flex justify-content-center align-items-center"
                         >
                           <img
                             alt={member.membername}
                             src={`${member.image}`}
                             className="member-photo" />
-                          
-                            <div className="member-photo-overlay">
+                          <div className="member-photo-overlay">
+
+                          </div>
+                          <div className="member-text-overlay">
                             <h6 className="montserrat">
                               {member.membername}
                             </h6>
@@ -55,7 +56,7 @@ function LabmembersSection() {
                               {member.functionbasic}
                             </p>
                           </div>
-                          </div>
+                        </div>
                       </Link>
 
                     </Col>
@@ -68,7 +69,7 @@ function LabmembersSection() {
               </>
             );
           })}
-{/* Current members Section*/}
+          {/* Current members Section*/}
           <h2 className="p-5 montserrat">Current members</h2>
           {labmemberList.map((member, index) => {
             return (
@@ -83,14 +84,18 @@ function LabmembersSection() {
                       xl={3}
                       className="p-3 d-flex justify-content-center align-items-center"
                     >
-                      <div className="text-center">
+
                         <Link to={`/labmember/${member.membername.replace(/\s/g, '-').toLowerCase()}`} className="photo-link">
-                          <div className=" members d-flex justify-content-center align-items-center">
-                            <img
-                              src={`${member.image}`}
-                              className="member-photo" alt={member.membername}
-                            />
-                     <div className="member-photo-overlay">
+                        <div className=" members d-flex justify-content-center align-items-center"
+                        >
+                          <img
+                            alt={member.membername}
+                            src={`${member.image}`}
+                            className="member-photo" />
+                          <div className="member-photo-overlay">
+
+                          </div>
+                          <div className="member-text-overlay">
                             <h6 className="montserrat">
                               {member.membername}
                             </h6>
@@ -98,10 +103,9 @@ function LabmembersSection() {
                               {member.functionbasic}
                             </p>
                           </div>
-                          </div>
-                   
+                        </div>
                         </Link>
-                    </div>
+
 
                     </Col>
 
@@ -113,7 +117,7 @@ function LabmembersSection() {
             );
           })}
           {/* Alumni Section*/}
-          <Link to={"/alumni"} className="p-5"><h3 className="d-flex"><Badge  className="badge-icon">SEE ALUMNI <BsFillArrowRightCircleFill  className="arrow-icon" /></Badge></h3></Link>
+          <Link to={"/alumni"} className="p-5"><h3 className="d-flex"><Badge className="badge-icon">SEE ALUMNI <BsFillArrowRightCircleFill className="arrow-icon" /></Badge></h3></Link>
         </Row>
       </Container>
     </>
--- Client/src/HompageComponents/ProjectsSction.js
+++ Client/src/HompageComponents/ProjectsSction.js
@@ -1,28 +1,36 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { Button, Card, Col, Container, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 function ProjectsSction() {
   const [projects, setProjects] = useState([]);
+  const selectedProject = useRef(null)
+
+
+
+
+
   const fetchProjectList = async () => {
     let res = await fetch("http://localhost:3001/projects/fetch-projects");
     let data = await res.json();
     console.log(data);
     setProjects(data);
+
   };
   useEffect(() => {
     fetchProjectList();
-  }, []);
+  }, [selectedProject]);
+
 
   return (
     <>
       <Container>
-        <h1 className="text-center p-5 lab-title">Research projects</h1>
+        <h1 className="text-left lab-title">Research projects</h1>
         <div className="d-flex flex-wrap justify-content-center">
           <Row>
-          {projects.map((project) => (
-            <>
-             {/* <div className="m-3">
+             {projects.map((project, e) => (
+              <>
+                {/* <div className="m-3">
              <Card  className="project-card">
               <Link to={`/project/${project.title.replace(/\s/g, '-').toLowerCase()}`}>
               <Card.Img variant="top" className="" src={project.image} />
@@ -34,27 +42,32 @@ function ProjectsSction() {
               </Card.Body>
             </Card> 
             </div> */}
-            <Col md={4}>
-   
-              <div className=" projects d-flex justify-content-center align-items-center">
-                <img
-                  src={project.image}
-                  className="project-photo" alt={project.title}
-                />
-                <div className="project-photo-overlay">
-          
-                  <p className="roboto">
-                  {project.title}
-                  </p>
-                </div>
-              </div>
-      
-            </Col>
-            </>
-          ))}
+                <Col md={4} className='row-elem' ref={selectedProject} onClick={() => console.log(selectedProject.current)}>
+                <Link to={`/project/${project.title.replace(/\s/g, '-').toLowerCase()}`} className="project-link">
+                  <div className=" projects d-flex justify-content-center" >
+                    <img
+                      src={project.image}
+                      className="project-photo" alt={project.title}
+                    />
+                    <div className="project-photo-overlay"></div>
+                    <div className="project-text-overlay">
+                      <p className="roboto">
+                        {project.title}
+                      </p>
+                    </div>
+                    <div className="project-summary-container" >
+                    <p className="roboto project-summary">
+                        {project.summary}.<br/> Read more ...
+                    </p>
+                    </div>
+                  </div>
+   </Link>
+                </Col>
+              </>
+            ))}
           </Row>
         </div>
-        
+
       </Container>
     </>
   );
