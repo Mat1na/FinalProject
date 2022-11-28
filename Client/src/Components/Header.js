466651aaf00ea784510f597f7785de7c17a7a9d1
Mat1na <Stamatia.Bampa@Student.intecbrussel.be>
2022-11-28 21:10:00 +0100

    navbar

--- Client/src/Components/Header.js
+++ Client/src/Components/Header.js
@@ -7,7 +7,7 @@ import { HashLink as Link } from 'react-router-hash-link';
 function Header() {
 
   window.addEventListener('scroll', function () {
-    if (window.scrollY > 300 && window.location.pathname ==='/' ) {
+    if (window.scrollY > 200 && window.location.pathname ==='/' ) {
       document
         .querySelector('.navbar')
         .classList.add('color-nav');
