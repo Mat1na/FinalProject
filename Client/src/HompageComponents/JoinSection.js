fd1a290d8e791fb40ca99665ff45b74f8d4cba70
Mat1na <Stamatia.Bampa@Student.intecbrussel.be>
2022-11-29 14:56:22 +0100

    footer absolut

--- Client/src/HompageComponents/JoinSection.js
+++ Client/src/HompageComponents/JoinSection.js
@@ -6,7 +6,7 @@ function JoinSection() {
     return (<>
 
 
-        <Container fluid className='mt-0 mb-0'>
+        <Container fluid className='mt-0 join'>
 
             <h1 className="lab-title pb-3 montserrat">Join the lab</h1>
             <div className='join-bg'>
