88ee270a9e06b462cbeb80c0216f76b600a02fb9
WinandVM <winand_vanmeerbeek@hotmail.com>
2022-12-04 15:55:17 +0100

    Footer restyling

--- Client/src/Components/Footer.js
+++ Client/src/Components/Footer.js
@@ -8,7 +8,7 @@ import { Link } from 'react-router-dom';
 
 function Footer() {
 
-  window.addEventListener('click',  (e)=> {
+  window.addEventListener('click', (e) => {
     if (window.location.pathname === '/login' || window.location.pathname === '/dashboard') {
       document
         .querySelector('.footer-bg')
@@ -18,7 +18,7 @@ function Footer() {
 
   });
 
-  window.addEventListener('load', (e)=>  {
+  window.addEventListener('load', (e) => {
     if (window.location.pathname === '/login' || window.location.pathname === '/dashboard') {
       document
         .querySelector('.footer-bg')
@@ -28,24 +28,44 @@ function Footer() {
 
   });
   return (
-    <div className="footer-bg">
-      <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4">
-        <div className="col-md-4 align-items-center">
-        <Link to='/' className='nav-link'><img src='/Logo_footer.png' alt='logo sGlobelab with tagline' className='logofooter mb-2' /></Link>
-          <span className="mb-3 mb-md-0 roboto d-block">&copy; 2022 <a href='https://www.linkedin.com/in/matinabampa/' target="_blank" rel="noreferrer" className='footerlink'>Stamatia Bampa</a> and <a href='https://www.linkedin.com/in/winandvanmeerbeek/' target="_blank" rel="noreferrer" className='footerlink'>Winand Van Meerbeek</a></span>
+    <div className="footer-bg mt-5">
+      <footer>
+        <div className="d-block justify-content-center">
+          <Link to='/' className='nav-link'><img src='/Logo_footer.png' alt='logo sGlobelab with tagline' className='logofooter'/></Link>
         </div>
-<div className='col-md-8'>
-        <ul className="nav justify-content-end list-unstyled d-flex">
-          <li className="ms-3"><GoMail size={"1rem"} className="icons me-1" /><a className="roboto footerlink" href={`mailto:koenraad.vanmeerbeek@kuleuven.be`}>koenraad.vanmeerbeek@kuleuven.be</a></li>
-          <li className="ms-3"><BsTelephoneFill size={"1rem"} className="icons me-1" /><a className="roboto footerlink" href="tel:+32%2016%20377444%20">+32 16 377 444</a></li>
-        </ul>
-        <ul className="nav justify-content-end list-unstyled d-flex">
-          <li className="ms-3"><FaTwitter size={"1rem"} className="icons me-1" /><a className="roboto footerlink" href="https://twitter.com/K_VanMeerbeek" target="_blank" rel="noreferrer">Follow me on Twitter</a></li>
-          <li className="ms-3"><FaMapMarkerAlt size={"1rem"} className="icons me-1" /><a className="roboto footerlink" href="https://www.google.be/maps/place/GEO-Instituut,+Celestijnenlaan+200E,+3001+Leuven/@50.8636993,4.6757642,493m/data=!3m2!1e3!4b1!4m5!3m4!1s0x47c16111ce8c3671:0x4ee0baa6fc29cae3!8m2!3d50.8636993!4d4.6757642" target="_blank" rel="noreferrer">GEO-Instituut, Celestijnenlaan 200E, 3001 Leuven</a></li>
-        </ul>
+        <div className='d-block footer-icon-list'>
+          <ul className="nav justify-content-center list-unstyled d-flex">
+            <li className="mx-3"><a className="footerlink" href={`mailto:koenraad.vanmeerbeek@kuleuven.be`}><GoMail size={"2.5rem"} className="icons footer-icons me-1" /></a></li>
+            <li className="mx-3"><a className="footerlink" href="tel:+32%2016%20377444%20"><BsTelephoneFill size={"2.5rem"} className="footer-icons icons me-1" /></a></li>
+            <li className="mx-3"><a className="footerlink" href="https://twitter.com/K_VanMeerbeek" target="_blank" rel="noreferrer"><FaTwitter size={"2.5rem"} className="footer-icons icons me-1" /></a></li>
+            <li className="mx-3"><a className="footerlink" href="https://www.google.be/maps/place/GEO-Instituut,+Celestijnenlaan+200E,+3001+Leuven/@50.8636993,4.6757642,493m/data=!3m2!1e3!4b1!4m5!3m4!1s0x47c16111ce8c3671:0x4ee0baa6fc29cae3!8m2!3d50.8636993!4d4.6757642" target="_blank" rel="noreferrer"><FaMapMarkerAlt size={"2.5rem"} className="footer-icons icons me-1" /></a></li>
+          </ul>
+        </div>
+        <div className='d-block text-center'>
+          <span className="roboto footer-copyright">&copy; 2022 <a href='https://www.linkedin.com/in/matinabampa/' target="_blank" rel="noreferrer" className='footerlink'>Stamatia Bampa</a> and <a href='https://www.linkedin.com/in/winandvanmeerbeek/' target="_blank" rel="noreferrer" className='footerlink'>Winand Van Meerbeek</a></span>
         </div>
       </footer>
     </div>
+
+
+    //     <div className="footer-bg">
+    //       <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4">
+    //         <div className="col-md-4 align-items-center">
+    //         <Link to='/' className='nav-link'><img src='/Logo_footer.png' alt='logo sGlobelab with tagline' className='logofooter mb-2' /></Link>
+    //           <span className="mb-3 mb-md-0 roboto d-block">&copy; 2022 <a href='https://www.linkedin.com/in/matinabampa/' target="_blank" rel="noreferrer" className='footerlink'>Stamatia Bampa</a> and <a href='https://www.linkedin.com/in/winandvanmeerbeek/' target="_blank" rel="noreferrer" className='footerlink'>Winand Van Meerbeek</a></span>
+    //         </div>
+    // <div className='col-md-8'>
+    //         <ul className="nav justify-content-end list-unstyled d-flex">
+    //           <li className="ms-3"><GoMail size={"1rem"} className="icons me-1" /><a className="roboto footerlink" href={`mailto:koenraad.vanmeerbeek@kuleuven.be`}>koenraad.vanmeerbeek@kuleuven.be</a></li>
+    //           <li className="ms-3"><BsTelephoneFill size={"1rem"} className="icons me-1" /><a className="roboto footerlink" href="tel:+32%2016%20377444%20">+32 16 377 444</a></li>
+    //         </ul>
+    //         <ul className="nav justify-content-end list-unstyled d-flex">
+    //           <li className="ms-3"><FaTwitter size={"1rem"} className="icons me-1" /><a className="roboto footerlink" href="https://twitter.com/K_VanMeerbeek" target="_blank" rel="noreferrer">Follow me on Twitter</a></li>
+    //           <li className="ms-3"><FaMapMarkerAlt size={"1rem"} className="icons me-1" /><a className="roboto footerlink" href="https://www.google.be/maps/place/GEO-Instituut,+Celestijnenlaan+200E,+3001+Leuven/@50.8636993,4.6757642,493m/data=!3m2!1e3!4b1!4m5!3m4!1s0x47c16111ce8c3671:0x4ee0baa6fc29cae3!8m2!3d50.8636993!4d4.6757642" target="_blank" rel="noreferrer">GEO-Instituut, Celestijnenlaan 200E, 3001 Leuven</a></li>
+    //         </ul>
+    //         </div>
+    //       </footer>
+    //     </div>
 
 
     // <div className="footer-bg mt-5 pt-0">
