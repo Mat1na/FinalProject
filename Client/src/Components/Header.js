d0690bf57fd52a3b2a64ec54b0bade4e87c016c2
WinandVM <winand_vanmeerbeek@hotmail.com>
2022-12-05 10:51:34 +0100

    Navbar close icon, height fix, etc.

--- Client/src/Components/Header.js
+++ Client/src/Components/Header.js
@@ -29,8 +29,11 @@ function Header() {
   return (
     <Navbar fixed="top" expand="lg" variant="dark" className="navbar p-2" >{  console.log(window.scrollY)}
       <Navbar.Brand href="/"><img src='/Logo_final_rgba.png' className='logoheader' alt="logo sGlobe Lab"/></Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav">
-        <MdOutlineSegment size={'2em'} fill='white' className="nav-icon" />
+      <Navbar.Toggle aria-controls="basic-navbar-nav" className='d-flex d-lg-none flex-column justify-content-around'>
+        <span className='toggler-icon top-bar'></span>
+        <span className='toggler-icon middle-bar'></span>
+        <span className='toggler-icon bottom-bar'></span>
+        {/* <MdOutlineSegment size={'2em'} fill='white' className="nav-icon" /> */}
         {/* <MdOutlineClose size={'2em'} fill='var(--Dark)'  className="social-icon" /> */}
       </Navbar.Toggle>
       <Navbar.Collapse id="basic-navbar-nav" className='justify-content-end'>
