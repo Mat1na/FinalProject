d6454dda7d3da27300389ec5832277859bab268a
Mat1na <Stamatia.Bampa@Student.intecbrussel.be>
2022-12-08 18:12:57 +0100

    2nd header update

--- Client/src/Components/Headermodified.js
+++ Client/src/Components/Headermodified.js
@@ -4,10 +4,10 @@ import { HashLink as Link } from 'react-router-hash-link';
 
 
 function Headermodified() {
-  const [expanded, setExpanded] =useState(false);
+  // const [expanded, setExpanded] =useState(false);
 
   return (
-    <Navbar collapseOnSelect fixed="top" expand={expanded} variant="dark" className="navbar p-2 color-nav" >
+    <Navbar collapseOnSelect fixed="top" expand='lg' variant="dark" className="navbar p-2 color-nav" >
     <Navbar.Brand href="/"><img src='/Logo_final_rgba.png' className='logoheader' alt="logo sGlobe Lab"/></Navbar.Brand>
     <Navbar.Toggle aria-controls="responsive-navbar-nav" className='d-flex d-lg-none flex-column justify-content-around'>
       <span className='toggler-icon top-bar'></span>
@@ -16,13 +16,13 @@ function Headermodified() {
     </Navbar.Toggle>
     <Navbar.Collapse id="responsive-navbar-nav" className='justify-content-end'>
       <Nav >
-        <Link to='/#top' className='nav-link' onClick={() => setExpanded(false)}>Home</Link>
-        <Link to="/#research" className='nav-link' onClick={() => setExpanded(false)}>Research</Link>
-        <Link to="/#labmembers" className='nav-link' onClick={() => setExpanded(false)}>Lab members</Link>
-        <Link to="/#projects" className='nav-link' onClick={() => setExpanded(false)}>Projects</Link>
-        <Link to="/#publications" className='nav-link' onClick={() => setExpanded(false)}>Publications</Link>
-        <Link to="/#jointhelab" className='nav-link' onClick={() => setExpanded(false)}>Join the lab</Link>
-        <Link to="/#contact" className='nav-link'onClick={() => setExpanded(false)}>Contact</Link>
+        <Link to='/#top' className='nav-link'>Home</Link>
+        <Link to="/#research" className='nav-link' >Research</Link>
+        <Link to="/#labmembers" className='nav-link'>Lab members</Link>
+        <Link to="/#projects" className='nav-link' >Projects</Link>
+        <Link to="/#publications" className='nav-link'>Publications</Link>
+        <Link to="/#jointhelab" className='nav-link'>Join the lab</Link>
+        <Link to="/#contact" className='nav-link'>Contact</Link>
       </Nav>
     </Navbar.Collapse>
 
