b9e124304466be9f51f453891be1c8d64d816c62
WinandVM <winand_vanmeerbeek@hotmail.com>
2022-12-08 14:03:50 +0100

    Quick header and image fix

--- Client/src/Components/Header.js
+++ Client/src/Components/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Nav, Navbar} from "react-bootstrap";
-import { HashLink as Link } from 'react-router-hash-link';
+// import { HashLink as Link } from 'react-router-hash-link';
 
 function Header() {
 
